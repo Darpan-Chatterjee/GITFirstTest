07aa70538cce6cefab0617de0e28891517c39978
DARPAN <DARPAN@Dell-PC>
2016-10-17 02:36:13 +0530

    some changes in code

--- GIT Demo1/src/GITDemo1.java
+++ GIT Demo1/src/GITDemo1.java
@@ -3,7 +3,8 @@ public class GITDemo1 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("HI..this is demo");
+		System.out.println("HI..this is demo1");
+		System.out.println("HI..this is demo2");
 
 	}
 
